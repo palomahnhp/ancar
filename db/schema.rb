bc0f68d319c7345f7e0a6cd718571dc772df7016
passcual <pascual.artigas@gmail.com>
2016-04-26 11:59:56 +0200

    Configuración de Capistrano

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 20160424115826) do
   add_index "main_processes", ["period_id"], name: "index_main_processes_on_period_id", using: :btree
   add_index "main_processes", ["unit_type_id"], name: "index_main_processes_on_unit_type_id", using: :btree
 
+  create_table "mainprocesses", force: :cascade do |t|
+    t.integer  "period_id"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "mainprocesses", ["period_id"], name: "index_mainprocesses_on_period_id", using: :btree
+
   create_table "organization_types", force: :cascade do |t|
     t.string   "acronym"
     t.string   "name"
@@ -124,6 +133,15 @@ ActiveRecord::Schema.define(version: 20160424115826) do
 
   add_index "sub_processes", ["main_process_id"], name: "index_sub_processes_on_main_process_id", using: :btree
 
+  create_table "subprocesses", force: :cascade do |t|
+    t.integer  "mainprocess_id"
+    t.string   "description"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "subprocesses", ["mainprocess_id"], name: "index_subprocesses_on_mainprocess_id", using: :btree
+
   create_table "tasks", force: :cascade do |t|
     t.string   "order"
     t.text     "description"
@@ -157,4 +175,5 @@ ActiveRecord::Schema.define(version: 20160424115826) do
   add_index "units", ["organization_id"], name: "index_units_on_organization_id", using: :btree
   add_index "units", ["unit_type_id"], name: "index_units_on_unit_type_id", using: :btree
 
+  add_foreign_key "subprocesses", "mainprocesses"
 end
