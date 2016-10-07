78999ae51cceb037452b87bf15a73a9fc45f2a41
palomahnhp <hernandezp@madrid.es>
2016-10-07 09:09:00 +0200

    change log an fix session development user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,13 @@
 require "uweb_authenticator"
-require 'logger'
-
-
 class SessionsController < ApplicationController
-  before_action :initialize_logger, only: [:create, :show]
 
   def create
     destroy_session
     if authenticated_uweb?
-      @logger.info('SessionControler#create: ') { "Sesion creada - #{params}" }
+      Rails.logger.info { "  INFO - SessionControler#create: Sesión creada - #{params}" }
       redirect_to root_path
     else
-      @logger.error('SessionControler#create: ') { "Sesion ERROR - #{params}" }
+      Rails.logger.error { "  ERROR - SessionControler#create: Sesión ERROR - #{params}" }
       redirect_to root_path, notice: "Usuario no autorizado"
     end
   end
@@ -32,28 +28,15 @@ class SessionsController < ApplicationController
         user = User.find_or_create_by(login: uw_user[:login])
         if user.uweb_update!(uw_user)
           session[:user_id] = user.id
+          Rails.logger.info { "  INFO - SessionControler#authenticated_uweb? Validación Uweb correcta para - #{user.login}" }
           return true
         else
-          @logger.error('SessionControler#authenticated_uweb? ') { "Falla comprobación Uweb - #{params}" }
+          Rails.logger.error { "  ERROR - SessionControler#user.uweb_update! Falla actualización user con datos Uweb - #{params}" }
           return false
         end
       else
-        @logger.error('SessionControler#authenticated_uweb? ') { "Falla comprobación Uweb - #{params}" }
-         user = User.find_by_login(params[:login])
-         if user
-           session[:user_id] = user.id
-           @logger.info('SessionControler#authenticated_uweb? ') { "Validación Uweb correcta para - #{user.login}" }
-           return true
-         else
-          @logger.error('SessionControler#authenticated_uweb? ') { "No encontrado usuario en BD:Users " }
-          return false
-         end
+        Rails.logger.error { "  ERROR - SessionControler#authenticated_uweb? Falla comprobación Uweb - #{params}" }
+         return false
       end
     end
-
-   def initialize_logger
-      @logger = Logger.new('session.log', 'weekly')
-      @logger.level = Logger::INFO
-    end
-
 end
