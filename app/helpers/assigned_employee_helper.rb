c433aac7f944783ed1b8b7177dd911605aae689b
palomahnhp <hernandezp@madrid.es>
2017-03-22 14:52:34 +0100

    fix_assigned_employees

--- app/helpers/assigned_employee_helper.rb
+++ app/helpers/assigned_employee_helper.rb
@@ -20,29 +20,60 @@ module AssignedEmployeeHelper
     end
   end
 
-  def get_staff(type, proc, group, unit, period)
-    ids =  proc.id
-    period_2015 = Period.find_by_description('PERIODO DE ANÁLISIS: AÑO 2015')
-    # unicamente el periodo de Distrito 2015 tiene datos en subprocesos en lugar de en indicadores
-    if type == 'SubProcess' &&  period != period_2015
+  def get_unit_staff(group, unit, period)
+
+    @period_2015 ||= Period.find_by_description('PERIODO DE ANÁLISIS: AÑO 2015')
+
+    if type == 'SubProcess' &&  period != @period_2015
       ids = proc.indicators.ids
       type = 'Indicator'
+    else
+      ids =  proc.id
     end
 
-    ae = AssignedEmployee.where(staff_of_id: ids, staff_of_type: type, official_group_id: group.id, unit_id: unit.id, period_id: period.id).count
-    if ae > 0
-      ae = AssignedEmployee.where(staff_of_id: ids, staff_of_type: type, official_group_id: group.id, unit_id: unit.id, period_id: period.id).sum(:quantity)
-    else
-      ae = nil
+    @unit_assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id).
+        group(:staff_of_type, :official_group_id).order(:staff_of_type, :official_group_id).
+        pluck(:staff_of_type, :official_group_id, 'count(id)', 'sum(quantity)')
+
+    @assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id, staff_of_id: ids).order(:staff_of_type, :staff_of_id, :staff_of_type, :official_group_id).
+        group(:staff_of_id, :staff_of_type, :official_group_id).
+        pluck(:staff_of_id, :staff_of_id, :official_group_id, 'count(id)', 'sum(quantity)')
+
+    if @assigned_employees.nil? &&  type == "UnitJustified"
+      type= 'Unit'
     end
-    if ae.nil?
-      if type == "UnitJustified"
-          ae = AssignedEmployee.where(staff_of_type: 'Unit', official_group_id: group.id, unit_id: unit.id, period_id: period.id).sum(:quantity)
-      elsif type == 'UnitAssigned'
-        ae = AssignedEmployee.where(staff_of_type: 'Indicator', official_group_id: group.id, unit_id: unit.id, period_id: period.id).sum(:quantity)
-      end
-    end
+
     return ae.nil? ? nil : format_number(ae)
+
+  end
+
+  def get_staff(type, proc, unit, period, class_of = '')
+
+    @period_2015 ||= Period.find_by_description('PERIODO DE ANÁLISIS: AÑO 2015')
+
+    if type == 'SubProcess' &&  period != @period_2015
+      ids = proc.indicators.ids
+      type = 'Indicator'
+    else
+      ids = []
+      ids <<  proc.id
+    end
+    if type == 'Unit'
+      @unit_assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id).order(:official_group_id).
+          group(:staff_of_type, :official_group_id).
+          pluck(:staff_of_type, :official_group_id, 0 , 'count(id)', 'sum(quantity)')
+      if class_of.empty?
+        return_array = @unit_assigned_employees.select{ |type_of, id, staff, amount| type_of == type }
+      elsif class_of == 'Assigned'
+        return_array = @unit_assigned_employees.select{ |type_of, id, staff, amount| (type_of == 'Indicator') }
+      end
+    else
+     @assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id).order(:official_group_id).
+        group(:staff_of_id, :staff_of_type, :official_group_id).
+        pluck(:staff_of_id, :staff_of_type, :official_group_id, 'count(id)', 'sum(quantity)')
+     return_array = @assigned_employees.select{ |id, type_of, staff, count, amount| ids.include?(id)  }
+    end
+     return return_array
   end
 
   def has_justification?(unit_id, period_id)
@@ -56,5 +87,9 @@ module AssignedEmployeeHelper
   def  justification_text(unit_id, period_id)
     ae = AssignedEmployeesChange.where(unit_id: unit_id, period_id: period_id).take
     ae.nil? ? '' : ae.justification
+  end
+
+  def official_groups
+    @official_groups ||=  OfficialGroup.all
   end
 end
