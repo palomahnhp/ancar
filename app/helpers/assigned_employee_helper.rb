3f6878a7c0baedb22b2a38aabb6a41301b529c14
palomahnhp <hernandezp@madrid.es>
2017-03-22 23:12:36 +0100

    fix_assigned_employees_optimization

--- app/helpers/assigned_employee_helper.rb
+++ app/helpers/assigned_employee_helper.rb
@@ -20,33 +20,6 @@ module AssignedEmployeeHelper
     end
   end
 
-  def get_unit_staff(group, unit, period)
-
-    @period_2015 ||= Period.find_by_description('PERIODO DE ANÁLISIS: AÑO 2015')
-
-    if type == 'SubProcess' &&  period != @period_2015
-      ids = proc.indicators.ids
-      type = 'Indicator'
-    else
-      ids =  proc.id
-    end
-
-    @unit_assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id).
-        group(:staff_of_type, :official_group_id).order(:staff_of_type, :official_group_id).
-        pluck(:staff_of_type, :official_group_id, 'count(id)', 'sum(quantity)')
-
-    @assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id, staff_of_id: ids).order(:staff_of_type, :staff_of_id, :staff_of_type, :official_group_id).
-        group(:staff_of_id, :staff_of_type, :official_group_id).
-        pluck(:staff_of_id, :staff_of_id, :official_group_id, 'count(id)', 'sum(quantity)')
-
-    if @assigned_employees.nil? &&  type == "UnitJustified"
-      type= 'Unit'
-    end
-
-    return ae.nil? ? nil : format_number(ae)
-
-  end
-
   def get_staff(type, proc, unit, period, class_of = '')
 
     @period_2015 ||= Period.find_by_description('PERIODO DE ANÁLISIS: AÑO 2015')
@@ -63,15 +36,15 @@ module AssignedEmployeeHelper
           group(:staff_of_type, :official_group_id).
           pluck(:staff_of_type, :official_group_id, 0 , 'count(id)', 'sum(quantity)')
       if class_of.empty?
-        return_array = @unit_assigned_employees.select{ |type_of, id, staff, amount| type_of == type }
+        return_array = @unit_assigned_employees.select{ |t| t[0] == type }
       elsif class_of == 'Assigned'
-        return_array = @unit_assigned_employees.select{ |type_of, id, staff, amount| (type_of == 'Indicator') }
+        return_array = @unit_assigned_employees.select{ |t| t[0] == 'Indicator'}
       end
     else
      @assigned_employees ||= AssignedEmployee.where(unit_id: unit.id, period_id: period.id).order(:official_group_id).
         group(:staff_of_id, :staff_of_type, :official_group_id).
         pluck(:staff_of_id, :staff_of_type, :official_group_id, 'count(id)', 'sum(quantity)')
-     return_array = @assigned_employees.select{ |id, type_of, staff, count, amount| ids.include?(id)  }
+     return_array = @assigned_employees.select{ |t| ids.include?(t[0])  }
     end
      return return_array
   end
