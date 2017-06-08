f82d37a77b47c6b12c1fddce5dbe298201424526
palomahnhp <hernandezp@madrid.es>
2017-06-08 11:08:56 +0200

    fix migration and change task capistrano compile_assests

--- db/migrate/20170531105311_add_cols_to_users.rb
+++ db/migrate/20170531105311_add_cols_to_users.rb
@@ -4,6 +4,6 @@ class AddColsToUsers < ActiveRecord::Migration
     add_column :users, :sap_den_unit, :string
     add_column :users, :sap_id_organization, :integer
     add_column :users, :sap_den_organization, :string
-    delete_column :users, :roles
+    remove_column :users, :roles
   end
 end
