6d1a7e959819a856b669bc70764d45c4f8f8c11b
palomahnhp <hernandezp@madrid.es>
2017-08-11 10:37:19 +0200

    traza en actualización imported amount

--- app/controllers/entry_indicators_controller.rb
+++ app/controllers/entry_indicators_controller.rb
@@ -121,128 +121,128 @@ class EntryIndicatorsController < ApplicationController
   end
 
   def validate_input
-      if params[:close_entry].present? || params[:approval].present?
-
-        @input_errors[:assignated_staff]     = AssignedEmployee.staff_for_unit(@period, @unit)
-        @input_errors[:entry_without_staff]  = Indicator.validate_staff_for_entry(@period, @unit)
-        @input_errors[:entry_incomplete]     = entry_incompleted?
-        @input_errors[:in_out_stock]         = SubProcess.validate_in_out_stock(@period, @unit)
-        @input_errors[:incomplete_staff_entry] = @incomplete_staff_entry
-      end
+    if params[:close_entry].present? || params[:approval].present?
+      @input_errors[:assignated_staff]     = AssignedEmployee.staff_for_unit(@period, @unit)
+      @input_errors[:entry_without_staff]  = Indicator.validate_staff_for_entry(@period, @unit)
+      @input_errors[:entry_incomplete]     = entry_incompleted?
+      @input_errors[:in_out_stock]         = SubProcess.validate_in_out_stock(@period, @unit)
+      @input_errors[:incomplete_staff_entry] = @incomplete_staff_entry
+    end
 
     if params[:approval].present?
-        @input_errors[:entry_incomplete]     = !(entry_indicators_cumplimented? && @incomplete_staff_entry.blank?)
-      end
-
-      @input_errors[:incomplete_staff_unit]  = @incomplete_staff_unit
-      @input_errors[:justification_blank]    = @justification_blank
-      if @input_errors[:justification_blank] && @input_errors[:incomplete_staff_unit].count == OfficialGroup.count
-        @input_errors[:cancel_change] = true
-        @input_errors[:incomplete_staff_unit]  = nil
-        @input_errors[:justification_blank]    = nil
-        @input_errors[:assignated_staff] = nil
-      end
-      @input_errors[:num_errors] = @input_errors.select{|error| @input_errors[error].present?}.count
+      @input_errors[:entry_incomplete]     = !(entry_indicators_cumplimented? && @incomplete_staff_entry.blank?)
     end
 
-    def entry_indicator_params
-      params.require(:entry_indicator).permit(:imported_amount, :amount, :unit_id, :period_id, :indicator_metric, :indicator_source)
+    @input_errors[:incomplete_staff_unit]  = @incomplete_staff_unit
+    @input_errors[:justification_blank]    = @justification_blank
+    if @input_errors[:justification_blank] && @input_errors[:incomplete_staff_unit].count == OfficialGroup.count
+      @input_errors[:cancel_change] = true
+      @input_errors[:incomplete_staff_unit]  = nil
+      @input_errors[:justification_blank]    = nil
+      @input_errors[:assignated_staff] = nil
+    end
+    @input_errors[:num_errors] = @input_errors.select{|error| @input_errors[error].present?}.count
+  end
+
+  def entry_indicator_params
+    params.require(:entry_indicator).permit(:imported_amount, :amount, :unit_id, :period_id, :indicator_metric, :indicator_source)
+  end
+
+  def update_indicator_metrics(indicator_metrics)
+    Indicator.includes(indicator_metrics: [:entry_indicators, :total_indicators])
+
+    indicator_metrics.each do |indicator|
+      indicator[1].each do |im|
+        indicator_metric_id = im[0].to_i
+        amount = im[1]
+
+        if amount.empty?
+          delete_entry_indicators(@unit.id, indicator_metric_id)
+          @entry_indicators_cumplimented = false
+        else
+          ei = EntryIndicator.find_or_create_by(unit_id: @unit.id, indicator_metric_id: indicator_metric_id)
+          ei.amount = amount.tr(',', '.').to_f
+          ei.imported_amount = ei.amount if ((current_user.has_role? :admin) && (source_imported?(ei.indicator_metric)))
+          logger.debug " -> amount = #{ei.amount}, imported_amount = #{ei.imported_amount}: imported_amount_save if admin ==  #{current_user.has_role? :admin}, && fuente es importada:  #{(source_imported?(ei.indicator_metric))} del indicator_metric = #{ei.indicator_metric.id}"
+          ei.period_id = @period.id
+          ei.updated_by = current_user.login
+          ei.save
+        end
+      end
+    end
+    return @entry_indicators_cumplimented
+  end
+
+  def initialize_instance_vars
+    @input_errors = Hash.new()
+    @incomplete_staff_unit = []
+    @incomplete_staff_entry = {}
+
+    if params[:organization_id]
+      @organization = Organization.find(params[:organization_id])
+      @units = @organization.units.order(:order).to_a
+    end
+    if params[:period_id]
+      @period = Period.includes(:assigned_employees, :entry_indicators, main_processes: [sub_processes: [tasks: [indicators:[
+      indicator_metrics: [metric: [:item]],
+      indicator_sources: [source: [:item]],
+      total_indicators: [:summary_type] ]]]]).find(params[:period_id]) if params[:period_id]
+    end
+    if params[:unit_id]
+      @unit = Unit.find(params[:unit_id])
+    else
+      @unit = @units.first
     end
 
-    def update_indicator_metrics(indicator_metrics)
-      Indicator.includes(indicator_metrics: [:entry_indicators, :total_indicators])
+    @approval = get_approval(@period, @unit)
+  end
 
-      indicator_metrics.each do |indicator|
-        indicator[1].each do |im|
-          indicator_metric_id = im[0].to_i
-          amount = im[1]
+  def entry_incompleted?
+    !(@entry_indicators_cumplimented && @incomplete_staff_entry)
+  end
 
-          if amount.empty?
-            delete_entry_indicators(@unit.id, indicator_metric_id)
-            @entry_indicators_cumplimented = false
+  def assigned_employees_update(type, process)
+    employees_cumplimented = true
+
+    process.each do |pr|
+      grupos = pr[1]
+      process_id = pr[0].to_i
+      if type == 'Unit'
+        type = 'UnitJustified'
+      end
+      grupos.keys.each do |grupo|
+        quantity = grupos[grupo]
+        official_group_id = OfficialGroup.find_by_name(grupo).id
+        if quantity.blank?
+          employees_cumplimented = false
+          if type == 'UnitJustified'
+            @incomplete_staff_unit <<  grupo
           else
-            ei = EntryIndicator.find_or_create_by(unit_id: @unit.id, indicator_metric_id: indicator_metric_id)
-            ei.amount = amount.gsub(',', '.').to_f
-            ei.imported_amount = ei.amount if current_user.has_role? :admin && source_imported?(ei.indicator_metric)
-            ei.period_id = @period.id
-            ei.updated_by = current_user.login
-            ei.save
+            @incomplete_staff_entry[process_id.to_s] = []
+            @incomplete_staff_entry[process_id.to_s] << grupo
           end
+          delete_assigned_employee(official_group_id, type, process_id, @period.id, @unit.id)
+        else
+          ae = set_assigned_employee(official_group_id, type, process_id, @period.id, @unit.id)
+          ae.quantity = quantity
+          ae.updated_by = current_user.login
+          ae.save
         end
       end
-      return @entry_indicators_cumplimented
     end
+    return employees_cumplimented
+  end
 
-    def initialize_instance_vars
-      @input_errors = Hash.new()
-      @incomplete_staff_unit = []
-      @incomplete_staff_entry = {}
-
-      if params[:organization_id]
-        @organization = Organization.find(params[:organization_id])
-        @units = @organization.units.order(:order).to_a
-      end
-      if params[:period_id]
-        @period = Period.includes(:assigned_employees, :entry_indicators, main_processes: [sub_processes: [tasks: [indicators:[
-        indicator_metrics: [metric: [:item]],
-        indicator_sources: [source: [:item]],
-        total_indicators: [:summary_type] ]]]]).find(params[:period_id]) if params[:period_id]
-      end
-      if params[:unit_id]
-        @unit = Unit.find(params[:unit_id])
-      else
-        @unit = @units.first
-      end
-
-      @approval = get_approval(@period, @unit)
-    end
-
-    def entry_incompleted?
-      !(@entry_indicators_cumplimented && @incomplete_staff_entry)
-    end
-
-    def assigned_employees_update(type, process)
-      employees_cumplimented = true
-
-      process.each do |pr|
-        grupos = pr[1]
-        process_id = pr[0].to_i
-        if type == 'Unit'
-          type = 'UnitJustified'
-        end
-        grupos.keys.each do |grupo|
-          quantity = grupos[grupo]
-          official_group_id = OfficialGroup.find_by_name(grupo).id
-          if quantity.blank?
-            employees_cumplimented = false
-            if type == 'UnitJustified'
-              @incomplete_staff_unit <<  grupo
-            elsif
-              @incomplete_staff_entry[process_id.to_s] = []
-              @incomplete_staff_entry[process_id.to_s] << grupo
-            end
-            delete_assigned_employee(official_group_id, type, process_id, @period.id, @unit.id)
-          else
-            ae = set_assigned_employee(official_group_id, type, process_id, @period.id, @unit.id)
-            ae.quantity = quantity
-            ae.updated_by = current_user.login
-            ae.save
+  def entry_indicators_cumplimented?
+    indicators_period = @period.indicators(@unit)
+    indicators_period.each do |indicator_subprocess|
+      indicator_subprocess.each do |indicator|
+          indicator.indicator_metrics.each do |indicator_metric|
+            return false if indicator_metric.entry_indicators.where(unit: @unit).blank?
           end
-        end
       end
-      return employees_cumplimented
     end
-
-    def entry_indicators_cumplimented?
-      indicators_period = @period.indicators(@unit)
-      indicators_period.each do |indicator_subprocess|
-        indicator_subprocess.each do |indicator|
-            indicator.indicator_metrics.each do |indicator_metric|
-              return false if indicator_metric.entry_indicators.where(unit: @unit).blank?
-            end
-        end
-      end
-      return true
-    end
+    return true
+  end
 
 end
