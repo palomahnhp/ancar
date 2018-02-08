5c188f2fe2a1ebac528ac62259852c5b2f9ca84a
palomahnhp <hernandezp@madrid.es>
2018-02-08 15:11:26 +0100

    send Mail open change only if valitations ok

--- app/mailers/supervisor_mailer.rb
+++ app/mailers/supervisor_mailer.rb
@@ -11,8 +11,13 @@ class SupervisorMailer < ApplicationMailer
       bcc = Setting.find_by_key("app_email").value
       to = Setting.find_by_key("supervisor.email.#{@period.organization_type.acronym}").value
       subject = "Cambio de plantilla: #{@unit.description_sap} / #{@unit.organization.description} "
-      mail(to: "<#{to}>", subject: subject, bcc: bcc )
-      AssignedEmployeesChange.emailed(@period.id, @unit.id) if data[:change] == 'open'
+      begin
+        mail(to: "<#{to}>", subject: subject, bcc: bcc )
+        AssignedEmployeesChange.emailed(@period.id, @unit.id) if data[:change] == 'open'
+      rescue  Exception => exception
+        puts 'Error enviando correo: ' + subject + ' - ' + exception.to_s
+      end
+
     end
   end
 end
