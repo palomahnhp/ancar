fbde23f2fe4946354e77419a3859afc11a272782
palomahnhp <hernandezp@madrid.es>
2018-02-28 14:47:01 +0100

    Import Rpt from sqlServer Gpwrpt

--- app/models/gpwrpt.rb
+++ app/models/gpwrpt.rb
@@ -1,15 +1,4 @@
 class Gpwrpt  < ActiveRecord::Base
   establish_connection GPWRPT_DB
-=begin
-  establish_connection({
-      adapter: 'sqlserver',
-      mode: 'dblib',
-      host: '10.90.51.153',
-      port: '2685',
-      username: 'GPNUSUU',
-      password: 'jg17PR9x',
-      database: 'GPWRPT'
-                       })
-=end
   self.table_name = 'gpw.rpt001'
 end
--- app/models/rpt.rb
+++ app/models/rpt.rb
@@ -26,8 +26,25 @@ class Rpt < ActiveRecord::Base
     end
   end
 
-  def self.import(file)
-    return false unless file.present?
+  def self.import(year, file='')
+    if file.present?
+      self.import_from_xls(file)
+    else
+      self.import_from_rptdb(year)
+    end
+  end
+
+  def self.import_from_rptdb(year)
+    Gpwrpt.connection
+    Organization.all.each do |organization|
+      sql_import_rpt_sentence(year, organiztion)
+      rpt_data = Gpwrpt.find_by_sql(sql_import_rpt_sentence(year, organization))
+      update_rpt_data(rpt_data)
+    end
+
+  end
+
+  def self.import_from_xls(file)
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
@@ -48,6 +65,58 @@ class Rpt < ActiveRecord::Base
       when ".xls" then Roo::Excel.new(file.path)
       when ".xlsx" then Roo::Excelx.new(file.path)
       else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+  private
+
+  def sql_import_rpt_sentence(year, organization)
+    fecha_baja = year.to_s + '1231'
+    fecha_actualiz = (year + 1).to_s + '0101'
+
+   "WITH PADRES(ID_UNIDAD, ID_UNIDAD_PADRE)
+    AS
+    (
+      SELECT unidad.ID_UNIDAD, unidad.ID_UNIDAD_PADRE
+        FROM DIRECTORIO.CEMI.GPWA200 unidad
+      -- Organización a procesar
+        WHERE unidad.ID_UNIDAD = #{organization.id})
+
+      UNION ALL
+
+      SELECT HIJA.ID_UNIDAD, HIJA.ID_UNIDAD_PADRE
+      FROM DIRECTORIO.GPW.ORGANICO_DIR HIJA, PADRES PADRE
+      WHERE HIJA.ID_UNIDAD_PADRE = PADRE.ID_UNIDAD
+    )
+    -- Datos de rpt de personal de las unidades seleccionadas antes
+    -- Cambiar idsap_organizacion a la organización de la que se están tratando las unidades
+    select #{year} as year, #{organization.description} as den_organizacion,
+           #{organization.id} as sapid_organizacion,
+           rpt.id_unidad as sapid_unidad, den_unidad,
+           fecha_baja, fecha_actualiz, id_puesto, den_puesto,
+           grupo_personal, grupo_personal_txt, area_personal, area_personal_txt, grtit_per, grtit_pto,
+           situacion, modalidad,
+           nombre, apellido1, apellido2,
+           sociedad, division,
+           status_pto_txt, editable_Z01, ocupada
+    from GPWRPT.gpw.rpt001 as rpt
+    join directorio.gpw.gpwa200 as directorio on rpt.id_unidad = directorio.id_unidad
+    where sociedad = 'MDRD' and
+        -- Las bajas anteriores a 31/12 no las tiene en cuenta
+        (fecha_baja > '#{fecha_baja}'  OR fecha_baja = '99991231')  and
+        fecha_actualiz < '#{fecha_actualiz}'  and
+        rpt.id_unidad in (SELECT  u.ID_UNIDAD
+    FROM  DIRECTORIO.GPW.ORGANICO_DIR u
+    WHERE u.ID_UNIDAD IN (SELECT DISTINCT ID_UNIDAD FROM PADRES)
+    )
+    order by grtit_per, den_unidad
+    ;"
+
+  end
+
+  def update_rpt_data (data)
+    data.each do |datum|
+      p datum
     end
   end
 
