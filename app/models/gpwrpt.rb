4fa41c772b856722b821e195694e66d53d338e46
palomahnhp <hernandezp@madrid.es>
2018-02-28 12:22:48 +0100

    Connect SqlServer Gpwrpt

--- app/models/gpwrpt.rb
+++ app/models/gpwrpt.rb
@@ -1,3 +1,15 @@
 class Gpwrpt  < ActiveRecord::Base
-  establish_connection FOURTH_DIMENSION_DB
+  establish_connection GPWRPT_DB
+=begin
+  establish_connection({
+      adapter: 'sqlserver',
+      mode: 'dblib',
+      host: '10.90.51.153',
+      port: '2685',
+      username: 'GPNUSUU',
+      password: 'jg17PR9x',
+      database: 'GPWRPT'
+                       })
+=end
+  self.table_name = 'gpw.rpt001'
 end
