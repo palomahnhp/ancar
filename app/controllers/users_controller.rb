cedd2466755c1d346dd89d7bc96bf680e8f042d4
palomahnhp <hernandezp@madrid.es>
2018-03-04 21:27:54 +0100

    shared user maintenance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
-
-#  after_create :assign_default_role
+  after_create :assign_default_role
 
   def create
     @user = User.new(user_params)
-    user = params[:user]
-    @user.login = user[:login]
-    @user.uweb_id = user[:clave_ind]
-    @user.name = user[:nombre_usuario]
-    @user.surname = user[:apellido1_usuario]
-    @user.second_surname = user[:apellido2_usuario]
+    user  = params[:user]
+
+    @user.login           = user[:login]
+    @user.uweb_id         = user[:clave_ind]
+    @user.name            = user[:nombre_usuario]
+    @user.surname         = user[:apellido1_usuario]
+    @user.second_surname  = user[:apellido2_usuario]
     @user.document_number = user[:dni]
-    @user.document_type = ' '
-    @user.phone = user[:telefono]
-    @user.pernr = user[:numero_personal]
-    @user.email = user[:mail]
-    @user.adscripcion = user[:unidad]
+    @user.document_type   = ' '
+    @user.phone           = user[:telefono]
+    @user.pernr           = user[:numero_personal]
+    @user.email           = user[:mail]
+    @user.adscripcion     = user[:unidad]
     @user.official_position = user[:cargo]
 
     if @user.save then
