a30cf0dba1b680638002d29262ee0408d6a47655
palomahnhp <hernandezp@madrid.es>
2018-04-10 14:15:27 +0200

    add trace to rpt importer

--- lib/importers/rpt_importer.rb
+++ lib/importers/rpt_importer.rb
@@ -3,6 +3,7 @@ module Importers
     def parse
       Rails.logger.info (self.class.to_s + ' - '  + Time.zone.now.to_s + " - comienza lectura fichero: #{@filepath}")
       spreadsheet = open_spreadsheet
+      Rails.logger.info (self.class.to_s + ' - '  + Time.zone.now.to_s + " - fichero leido: #{@filepath} " + spreadsheet.last_row.to_s + ' filas' )
       header = spreadsheet.row(1)
       (2..spreadsheet.last_row).each do |i|
         row = Hash[[header, spreadsheet.row(i)].transpose]
@@ -11,6 +12,7 @@ module Importers
         rpt.attributes   = row.to_hash
         rpt.organization = FirstLevelUnit.find_by(sapid_unit: row["sapid_area"]).organization
         rpt.unit         = Unit.find_by(sap_id: row["sapid_unidad"]).presence
+        Rails.logger.info (self.class.to_s + ' - '  + Time.zone.now.to_s + " - procesado registro: " + i.to_s)
         begin
           rpt.save!
         rescue
